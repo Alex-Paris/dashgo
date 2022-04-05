453fbf70552cd9b7af651ffe1abd839e3569c7ed
Alex Paris <alexlima@gruposgapetro.com.br>
2022-04-05 20:38:45 -0300

    feat: apex chart

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,76 @@
-import { Flex } from '@chakra-ui/react';
+import { Box, Flex, SimpleGrid, Text, theme } from '@chakra-ui/react';
+import { ApexOptions } from 'apexcharts';
+import dynamic from 'next/dynamic';
 
 import Header from '../components/Header'
 import Sidebar from '../components/Sidebar';
+
+const Chart = dynamic(() => import('react-apexcharts'), {
+  ssr: false
+})
+
+const options: ApexOptions = {
+  chart: {
+    toolbar: {
+      show: false
+    },
+    zoom: {
+      enabled: false
+    },
+    foreColor: theme.colors.gray[500]
+  },
+  grid: {
+    show: false
+  },
+  dataLabels: {
+    enabled: false
+  },
+  tooltip: {
+    enabled: false
+  },
+  xaxis: {
+    type: 'datetime',
+    axisBorder: {
+      color: theme.colors.gray[600]
+    },
+    axisTicks: {
+      color: theme.colors.gray[600]
+    },
+    // categories: [
+    //   '2021-03-18T00:00:00.000Z',
+    //   '2021-03-19T00:00:00.000Z',
+    //   '2021-03-20T00:00:00.000Z',
+    //   '2021-03-21T00:00:00.000Z',
+    //   '2021-03-22T00:00:00.000Z',
+    //   '2021-03-23T00:00:00.000Z',
+    //   '2021-03-24T00:00:00.000Z',
+    // ]
+  },
+  fill: {
+    opacity: 0.3,
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      opacityFrom: 0.7,
+      opacityTo: 0.3,
+    }
+  }
+};
+
+const series: ApexOptions['series'] = [
+  {
+    name: 'series1',
+    data: [
+      [new Date('2021-03-18').getTime(), 31],
+      [new Date('2021-03-19').getTime(), 120],
+      [new Date('2021-03-20').getTime(), 10],
+      [new Date('2021-03-21').getTime(), 28],
+      [new Date('2021-03-22').getTime(), 61],
+      [new Date('2021-03-23').getTime(), 18],
+      [new Date('2021-03-24').getTime(), 109]
+    ]
+  }
+];
 
 export default function Dashboard(): JSX.Element {
   return (
@@ -10,6 +79,28 @@ export default function Dashboard(): JSX.Element {
 
       <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
         <Sidebar />
+
+        <SimpleGrid flex="1" gap="4" minChildWidth="320px" alignItems="flex-start">
+          <Box
+            p="8"
+            bg="gray.800"
+            borderRadius={8}
+            pb="4"
+          >
+            <Text fontSize="lg" mb="4">Inscritos da semana</Text>
+            <Chart options={options} series={series} type='area' height={160} />
+          </Box>
+          <Box
+            p="8"
+            bg="gray.800"
+            borderRadius={8}
+            pb="4"
+          >
+            <Text fontSize="lg" mb="4">Taxa de abertura</Text>
+            <Chart options={options} series={series} type='area' height={160} />
+          </Box>
+
+        </SimpleGrid>
       </Flex>
     </Flex>
   );

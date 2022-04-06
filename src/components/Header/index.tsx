aea824a5a5a6e7e126052abd79aadabcc07f3cec
Alex Paris <alexlima@gruposgapetro.com.br>
2022-04-06 18:35:06 -0300

    chore: more responsive pages

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,16 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react';
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react';
+
 import { Logo } from './Logo';
 import { NotificationsNav } from './NotificationsNav';
 import { Profile } from './Profile';
 import { SearchBox } from './SearchBox';
 
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext';
+import { RiMenuLine } from 'react-icons/ri';
+
 export function Header(): JSX.Element {
+  const { onOpen } = useSidebarDrawer();
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -21,6 +27,17 @@ export function Header(): JSX.Element {
       px="6"
       align="center"
     >
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        />
+      )}
+
       <Logo />
 
       {isWideVersion && <SearchBox />}

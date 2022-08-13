bef0b873989caad99f12fc4cc94d617f56bb6882
Alex Paris <alex_paris28@hotmail.com>
2022-08-13 07:25:16 -0300

    feat: axios and hook to get users from api

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -17,38 +17,14 @@ import {
 } from '@chakra-ui/react';
 import Link from 'next/link';
 import { RiAddBoxLine, RiPencilLine } from 'react-icons/ri';
-import { useQuery } from '@tanstack/react-query';
 
 import { Pagination } from '../../components/Pagination';
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
+import { useUsers } from '../../services/hooks/useUsers';
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useQuery(
-    ['users'],
-    async () => {
-      const response = await fetch('http://localhost:3000/api/users');
-      const data = await response.json();
-
-      const users = data.users.map(user => {
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-            day: '2-digit',
-            month: 'long',
-            year: 'numeric',
-          }),
-        };
-      });
-
-      return users;
-    },
-    {
-      staleTime: 1000 * 60,
-    }
-  );
+  const { data, isLoading, isFetching, error } = useUsers();
 
   const isWideVersion = useBreakpointValue({
     base: false,
